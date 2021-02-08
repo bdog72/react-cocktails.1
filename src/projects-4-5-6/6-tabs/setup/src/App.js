b52b7ecde6b770116032199e31020fd22c79643b
Brian Bycynski <high5bri@hotmail.com>
2021-02-08 05:40:44 -0500

    working on tabs project

--- src/projects-4-5-6/6-tabs/setup/src/App.js
+++ src/projects-4-5-6/6-tabs/setup/src/App.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 //
 //
 
@@ -11,9 +10,58 @@ import './index.scss';
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project';
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+  // eslint-disable-next-line no-unused-vars
+  const [value, setValue] = useState([0]);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+
+    setJobs(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className='app__6-tabs-section section loading'>
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
+
+  const { company, dates, duties, title } = jobs[value];
+
   return (
     <div className='app__6-tabs'>
-      <h2>tabs project setup</h2>
+      <section className='section'>
+        <div className='title'>
+          <h2>experience</h2>
+          <div className='underline'></div>
+        </div>
+        <div className='jobs-center'>
+          {/* btn-container */}
+          {/* job-info */}
+          <article className='job-info'>
+            <h3>{title}</h3>
+            <h4>{company}</h4>
+            <p className='job-date'>{dates}</p>
+            {duties.map((duty, index) => {
+              return (
+                <div key={index} className='job-desc'>
+                  <FaAngleDoubleRight className='job-icon' />
+                  <p>{duty}</p>
+                </div>
+              );
+            })}
+          </article>
+        </div>
+      </section>
     </div>
   );
 }
